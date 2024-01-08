88a242359ccc6cf912a3f11dbd51530d17f60edc
Wa8De <morrocan.wade@gmail.com>
2024-01-08 21:05:06 +0100

    changes

--- routes/roles_permissions/PermissionsRoutes.js
+++ routes/roles_permissions/PermissionsRoutes.js
@@ -7,6 +7,7 @@ router.get("/permissions/:id", PermissionsController.getPermissions);
 router.post("/permissions/:id", PermissionsController.addPermission);
 router.put("/permissions/:id/", PermissionsController.editPermissions);
 router.delete("/permissions/:id/:perm_id", PermissionsController.deletePermissions);
+// router.delete("/permissions/:id/:perm_id", PermissionsController.deletePermissions);
 
 // export routers
 module.exports = router;
