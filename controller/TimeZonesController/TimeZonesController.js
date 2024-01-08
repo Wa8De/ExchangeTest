b42381908de446637d51d1ca66b2797ffa1d9bd9
Wa8De <morrocan.wade@gmail.com>
2024-01-08 21:04:38 +0100

    changes

--- controller/TimeZonesController/TimeZonesController.js
+++ controller/TimeZonesController/TimeZonesController.js
@@ -20,6 +20,23 @@ class TimeZoneController {
         return res.status(500).json({ error: "Internal Server Error" });
         }
     };
+    // static getTimeZones = async (req, res) => {
+    //     try {
+    //     const timezones = await TimeZone.find();
+    //     if (timezones) {
+    //         return res.status(200).json({
+    //         timezones,
+    //         });
+    //     } else {
+    //         return res.status(404).json({
+    //         message: "No Timezone found",
+    //         });
+    //     }
+    //     } catch (error) {
+    //     console.log(error);
+    //     return res.status(500).json({ error: "Internal Server Error" });
+    //     }
+    // };
 
 
 }
